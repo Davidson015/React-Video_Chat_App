e974ad25efc52625201e57a02d45e683ada24961
David Ogunsola <tobilobadiekola@gmail.com>
2021-09-24 23:29:35 +0100

    add: Added Images

--- client/src/App.js
+++ client/src/App.js
@@ -47,4 +47,8 @@ const App = () => {
   )
 }
 
+window.onload = () => {
+  alert("Directions for use:\n\n\n1. Once the website loads up, enable it permission to use your video and audio.\n\n2. After which you fill in your name.\n\n3. To call another User, the User would send you an ID which you would copy to paste on the Field requesting for 'ID to call'\n\tN:B. If you're the one wanting to be called; click on the 'Copy your ID' button which would immediately copy your ID to your device's Clipboard after which you send that ID to the other Person wanting to call you.\n\n\n\nP.S --> Once you Hang Up on a call, The Page Refreshes and generates a new user ID for you. Also, if you refresh the page for any reason, a new user ID will be generated.")
+};
+
 export default App
